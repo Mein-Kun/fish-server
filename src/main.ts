b753731b9783066f628fe89418b68233fa27229e
Denis <kbs.pstu@gmail.com>
2023-11-12 14:44:36 +0500

    add url 7

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,8 @@
+import { NestFactory } from '@nestjs/core';
 import * as session from 'express-session';
 import * as passport from 'passport';
-import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder } from '@nestjs/swagger';
-import { SwaggerModule } from '@nestjs/swagger/dist';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
